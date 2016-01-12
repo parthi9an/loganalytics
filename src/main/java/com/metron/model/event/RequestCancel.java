8bd07b1b33e10b906c144291c70bde5f69350e57
dileep <dileep@820c42a0-3ee1-43bc-9296-649e20e32c83>
2016-01-12 10:21:53 +0000

    Fixing Delta issue

--- src/main/java/com/metron/model/event/RequestCancel.java
+++ src/main/java/com/metron/model/event/RequestCancel.java
@@ -3,12 +3,15 @@ package com.metron.model.event;
 import java.util.Date;
 import java.util.HashMap;
 
+import com.metron.AppConfig;
 import com.metron.model.Request;
 import com.metron.orientdb.OrientUtils;
 import com.metron.util.TimeWindowUtil.DURATION;
 import com.metron.util.Utils;
 
 public class RequestCancel extends RequestEvent {
+    
+    private int maxRetries = AppConfig.getInstance().getInt("db.maxRetry");
 
     public RequestCancel(String[] eventData) {
         super(eventData);
@@ -72,9 +75,14 @@ public class RequestCancel extends RequestEvent {
         System.out.println("RequestCancle: props \t starttime is" + request.vertex.getProperty("startTime"));
         
         if(request.vertex.getProperty("startTime") == null){
+            while(maxRetries > 0){
             String parentId = this.getStringAttr("parentId");
             request = new Request(this.getStringAttr("requestId"), parentId, this.getGraph());
             System.out.println("RequestCancle: props retry \t starttime is" + request.vertex.getProperty("startTime"));
+            maxRetries--;
+            if (request.vertex.getProperty("startTime") != null)
+                break;
+            }
         }
         
         if (request.vertex.getProperty("startTime") != null) {
