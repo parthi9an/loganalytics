9f640a3680d25212fd5758e5ea3a2c8e54382d49
dileep <dileep@820c42a0-3ee1-43bc-9296-649e20e32c83>
2016-01-27 09:27:12 +0000

    Rest APi's to retrieve action names (action_key) & counts of commands/actions issued for each “action name"

---
+++ src/main/java/com/metron/event/service/ActionEventService.java
@@ -0,0 +1,68 @@
+package com.metron.event.service;
+
+import org.json.JSONObject;
+
+import com.metron.controller.QueryWhereBuffer;
+
+public class ActionEventService extends BaseEventService{
+
+    public JSONObject getCountOfCommandForAction(String actionKey, String sessionId,
+            String fromDate, String toDate) {
+        
+        JSONObject result = new JSONObject();
+        StringBuffer query = new StringBuffer();
+        QueryWhereBuffer whereClause = new QueryWhereBuffer();
+        
+        if (actionKey != null) {
+            whereClause.append("in.action_key ='" + actionKey + "'");
+        }
+        if (sessionId != null) {
+            whereClause.append("out.metric_session_id ='" + sessionId + "'");
+        }
+        if (fromDate != null) {
+            whereClause.append("metric_timestamp >= '" + fromDate + "' ");
+        }
+        if (toDate != null) {
+            whereClause.append("metric_timestamp <= '" + toDate + "' ");
+        }
+
+        query.append("select count(*) as count,in.action_command as name from Metric_Action group by in.action_command"
+                + ((!whereClause.toString().equals("")) ? " Where " + whereClause.toString() : ""));
+
+        result = this.getAssociatedCount(query.toString());
+
+        return result;
+    }
+
+    /**
+     * Returns Action names / action_key
+     * @param sessionId
+     * @param fromDate
+     * @param toDate
+     * @return
+     */
+    public JSONObject getActionNames(String sessionId, String fromDate, String toDate) {
+        
+        JSONObject result = new JSONObject();
+        StringBuffer query = new StringBuffer();
+        QueryWhereBuffer whereClause = new QueryWhereBuffer();
+        
+        if (sessionId != null) {
+            whereClause.append("out.metric_session_id ='" + sessionId + "'");
+        }
+        if (fromDate != null) {
+            whereClause.append("metric_timestamp >= '" + fromDate + "' ");
+        }
+        if (toDate != null) {
+            whereClause.append("metric_timestamp <= '" + toDate + "' ");
+        }
+
+        query.append("select distinct(in.action_key) as name from Metric_Action"
+                + ((!whereClause.toString().equals("")) ? " Where " + whereClause.toString() : ""));
+
+        result = this.getNames(query.toString());
+
+        return result;
+    }
+
+}
