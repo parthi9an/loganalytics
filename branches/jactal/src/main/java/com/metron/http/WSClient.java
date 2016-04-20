4affa6c24d67af9cf0c8a71172a3cc65006ad4a4
dileep <dileep@820c42a0-3ee1-43bc-9296-649e20e32c83>
2016-04-20 07:12:47 +0000

    Handling the OrientDB Connection Exceptions while sending the request's by retrying the request 'n'(default 10) times to fetch the response from server (orientdb).

--- branches/jactal/src/main/java/com/metron/http/WSClient.java
+++ branches/jactal/src/main/java/com/metron/http/WSClient.java
@@ -17,8 +17,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import com.metron.AppConfig;
+
 public class WSClient {
 
+    private int maxRetries = AppConfig.getInstance().getInt("db.maxRetry");
+    
 	public HttpResponseData get(String url) {
 		return this.get(url, null, null);
 	}
@@ -27,33 +31,38 @@ public class WSClient {
 		System.out.println("URL :"+url);
 		StringBuffer result = new StringBuffer();
 		Header[] headers = null;
-		try {
-			HttpClient client = new DefaultHttpClient();
-			url = url.replace(" ", "%20");
-			HttpGet request = new HttpGet(url);
-			if (username != null && password != null)
-				request.addHeader(BasicScheme.authenticate(new UsernamePasswordCredentials(
-						username, password), "UTF-8", false));
+		boolean retry = true;
+        while (retry && maxRetries > 0) {
+            try {
+                HttpClient client = new DefaultHttpClient();
+                url = url.replace(" ", "%20");
+                HttpGet request = new HttpGet(url);
+                if (username != null && password != null)
+                    request.addHeader(BasicScheme.authenticate(new UsernamePasswordCredentials(
+                            username, password), "UTF-8", false));
 
-			HttpResponse response;
+                HttpResponse response;
 
-			response = client.execute(request);
-			headers = response.getAllHeaders();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity()
-					.getContent()));
+                response = client.execute(request);
+                headers = response.getAllHeaders();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity()
+                        .getContent()));
 
-			result = new StringBuffer();
-			String line = "";
+                String line = "";
 
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+                retry = false; // break the loop;
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Connection Exception, Trying to connect to Orientdb server");
+                maxRetries--;
+                get(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 //		System.out.println(" ---- URL " + url);
 		HttpResponseData responseData = new HttpResponseData(result.toString(), headers);
 		return responseData;
